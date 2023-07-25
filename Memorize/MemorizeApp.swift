f8453c7da6dfd73c9fd1cf5c0ab91da84c090c58
Sebastian Tleye <stleye@ovloop.com>
2023-07-25 10:16:50 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sebastian Tleye on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
